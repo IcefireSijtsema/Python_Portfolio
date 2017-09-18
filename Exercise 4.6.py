1034e32de70f9b0c03e31d7d377b8821fcd0fc43
Hi <I.brand@hotmail.com>
2017-09-18 16:43:56 +0200

    Hi

---
+++ Exercise 4.6.py
@@ -0,0 +1,11 @@
+lijst = ["a", "b", "c"]
+
+def wijzig(letterlijst):
+    lijst.clear()
+    lijst.append("d")
+    lijst.append("e")
+    lijst.append("f")
+
+print(lijst)
+wijzig(lijst)
+print(lijst)
